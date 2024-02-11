92cbdd590aab920947509c27657afe1e0f767d89
hannesivask <hannesegmond@gmail.com>
2024-02-11 13:42:42 +0200

    Update: Update homepage, styled carousel, made movie page

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,12 @@
 import { API_URL, KEY } from "./config_temp.js";
 
-const grid = document.querySelector(".grid");
-const heroBackdrop = document.querySelector(".hero__image--movie");
-const heroPoster = document.querySelector(".hero__image--poster");
-const heroTitle = document.querySelector(".title");
-const heroYear = document.querySelector(".year");
-const heroSummary = document.querySelector(".summary");
+const newMovies = document.querySelector(".new-movies");
+const topMovies = document.querySelector(".top-movies");
+const heroBackdrop = document.querySelector(".image-box__img--movie");
+const heroPoster = document.querySelector(".image-box__img--poster");
+const heroTitle = document.querySelector(".hero-title");
+const heroYear = document.querySelector(".hero-year");
+const heroSummary = document.querySelector(".hero-summary");
 
 const options = {
   method: "GET",
@@ -18,26 +19,33 @@ const options = {
 const getMovie = async function () {
   try {
     const data = await fetch(
-      `${API_URL}/movie/top_rated?language=en-US&page=1`,
+      `${API_URL}/movie/popular?language=en-US&page=1`,
       options
     );
     const json = await data.json();
     console.log(json);
-    heroBackdrop.src = `https://image.tmdb.org/t/p/original${json.results[2].backdrop_path}`;
-    heroPoster.src = `https://image.tmdb.org/t/p/w500${json.results[2].poster_path}`;
 
     const date = new Date(json.results[2].release_date);
     const year = date.getFullYear();
-    heroTitle.textContent = json.results[2].original_title;
-    heroYear.textContent = year;
-    heroSummary.textContent = json.results[2].overview;
 
-    for (let i = 0; i < 5; i++) {
+    const random = Math.floor(Math.random() * 20);
+
+    console.log(random);
+    heroBackdrop.src = `https://image.tmdb.org/t/p/original${json.results[random].backdrop_path}`;
+    heroPoster.src = `https://image.tmdb.org/t/p/w500${json.results[random].poster_path}`;
+
+    heroTitle.textContent = json.results[random].title;
+    heroYear.textContent = year;
+    heroSummary.textContent = json.results[random].overview;
+
+    for (let i = 5; i < 10; i++) {
       const poster = json.results[i].poster_path;
-      const title = json.results[i].original_title;
+      const title = json.results[i].title;
       const rating = json.results[i].vote_average;
-      const markup = generateMarkup(rating, poster, title);
-      grid.insertAdjacentHTML("afterbegin", markup);
+      const id = json.results[i].id;
+      const markup = generateMarkup(rating, poster, title, id);
+      newMovies.insertAdjacentHTML("afterbegin", markup);
+      topMovies.insertAdjacentHTML("afterbegin", markup);
     }
   } catch (err) {
     console.log(err);
@@ -46,19 +54,21 @@ const getMovie = async function () {
 
 getMovie();
 
-const generateMarkup = function (rating, poster, title) {
+const generateMarkup = function (rating, poster, title, id) {
   return `
   <div class="card">
     <span class="card__rating">
     <svg class="card__rating-icon">
       <use href="/src/img/sprite.svg#icon-star-full"></use>
-    </svg>${rating.toFixed(2)}</span>
+    </svg>${rating.toFixed(1)}</span>
     <div class="card__img-box">
-      <img  
-        src="https://image.tmdb.org/t/p/w500${poster}"
-        alt="Poster image"
-        class="card__img"
-      />  
+      <a href="src/html/movie.html?id=${id}" class="card__link">  
+        <img  
+          src="https://image.tmdb.org/t/p/w500${poster}"
+          alt="Poster image"
+          class="card__img"
+        />
+      </a>  
     </div>
     <span class="card__title">${title}</span>
     <button class="btn btn--filled card__btn">&plus; watchlist</button>
